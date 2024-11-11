986dcc20ae04f57306f13fab620be4d272f4fe6d
akvaxthelich <andrewdinspechin@gmail.com>
2024-11-11 17:39:35 -0500

    Successfully counted pixels in needed area.

--- NaiveAlgo.py
+++ NaiveAlgo.py
@@ -1,3 +1,4 @@
+
 # CV: Team V. Simple Algorithm
 
 # first zoom in, given some width/height to the joint
@@ -9,3 +10,36 @@
 # naive algorithm: section area is arbitrary
 
 # Identify images with > difference in area between 
+from matplotlib import pyplot as plt
+import numpy as np
+
+from PIL import Image
+img = Image.open('test.png').convert('L')
+img2 = img
+img2 = img2.crop((120,170,240,224)) 
+#Left, Upper, Right, Lower
+# The right can also be represented as (left+width)
+# and lower can be represented as (upper+height).
+npimage = np.asarray(img)
+
+plt.imshow(npimage, cmap='summer', vmax = 255, vmin = 0, interpolation=None)
+plt.show() 
+
+npimage2 = np.asarray(img2)
+
+threshold = 220
+
+space = 0
+
+print(img2.width)
+print(img2.height)
+
+for x in range(img2.width):
+    for y in range(img2.height):
+        if(img2.getpixel((x,y)) > threshold):
+            space += 1
+
+plt.imshow(npimage2, cmap='summer', vmax = 255, vmin = 0, interpolation=None)
+plt.show() 
+
+print(space)
