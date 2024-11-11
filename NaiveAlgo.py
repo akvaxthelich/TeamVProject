4be66e6ee833e492ef65850130fb0259ba190b2b
akvaxthelich <andrewdinspechin@gmail.com>
2024-11-11 15:35:24 -0500

    Added workspace file and NAIVE Algo header.

---
+++ NaiveAlgo.py
@@ -0,0 +1,11 @@
+# CV: Team V. Simple Algorithm
+
+# first zoom in, given some width/height to the joint
+# top left most white pixel, start counting from top to bottom black pixels
+# until we hit another white pixel
+# add the number of pixels into float
+# divide by the width of the section
+
+# naive algorithm: section area is arbitrary
+
+# Identify images with > difference in area between 
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 This pedogogical computer vision project classifies Knee OA utilizing methods by Dr. Juan Shan.
+
+For Editors:
+Please open the TeamV.code-workspace file in VS Code.
